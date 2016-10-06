6a4b819ae6da0b4e02aee70e4f8d6fc8960c0eb3
avj2352 <avj2352@gmail.com>
2016-10-06 09:15:54 +0530

    Initial Repository

---
+++ ci_zenStudents/application/controllers/api.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Api extends REST_Controller{
+
+  public $data = array();
+
+  public function __construct(){
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }//end:construcutor
+
+  function index(){ }//end:index
+
+
+  //NOTE: All of our REST API calls are - resourceName_httpMethodCalls.
+  //NOTE: For a list applicable http calls - see line #172 of REST_Controller.php
+
+  function student_get(){
+    $this->data[1] = array('name' => 'Pramod', 'age'=>'29');
+    $this->data[2] = array('name' => 'Shwetha', 'age'=>'28');
+    $this->response($this->data);
+  }//end:get_students
+
+}/*end:ApiController*/
